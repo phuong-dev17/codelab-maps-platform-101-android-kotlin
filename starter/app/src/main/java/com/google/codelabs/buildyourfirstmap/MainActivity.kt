7df973489bd20c71a3873b62e546e160d218511e
phuong-dev17 <phuong.dev17@gmail.com>
2024-02-09 23:25:08 +0100

    Finish Load Map and add markers

--- starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
+++ starter/app/src/main/java/com/google/codelabs/buildyourfirstmap/MainActivity.kt
@@ -14,7 +14,44 @@
 
 package com.google.codelabs.buildyourfirstmap
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.codelabs.buildyourfirstmap.place.Place
+import com.google.codelabs.buildyourfirstmap.place.PlacesReader
 
 class MainActivity : AppCompatActivity() {
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(this, R.color.colorPrimary)
+        BitmapHelper.vectorToBitmap(this, R.drawable.ic_directions_bike_black_24dp, color)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+                    .icon(bicycleIcon)
+            )
+        }
+    }
 }
